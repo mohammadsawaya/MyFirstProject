67eff658d14cfe0e4c361b14a0c308912198f7df
Mohammad Sawaya <en.mas@hotmail.com>
2017-05-08 16:03:41 +0200

    With Package and Class

--- src/com/company/test/Main.java
+++ src/com/company/test/Main.java
@@ -1,9 +1,19 @@
 package com.company.test;
 
+import ClassesPackage.HauptClass;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+
+
+        HauptClass hc= new HauptClass();
+        hc.Addemplyee(1,"mohammad","en.mas@hotmail.com",01765553333);
+        hc.Printemployee();
     }
 }
+//jdk;java development kit
+//jri :java runtime einveroment
+//
